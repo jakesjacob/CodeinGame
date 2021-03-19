db005e6e7d597c504fea795ac66721782d98e7c3
jakesjacob <33692879+jakesjacob@users.noreply.github.com>
2021-03-19 21:25:33 +0000

    second

--- Temperatures.cpp
+++ Temperatures.cpp
@@ -13,12 +13,13 @@ using namespace std;
 int main()
 {
     int n; // the number of temperatures to analyse
-    int temps[n];
+
     int closestPos = 0;
     int closestNeg = 0;
     int closest = 0;
     cin >> n;
     cin.ignore();
+    int temps[n];
     for (int i = 0; i < n; i++)
     {
         int t; // a temperature expressed as an integer ranging from -273 to 5526
@@ -26,12 +27,22 @@ int main()
         cin.ignore();
 
         temps[i] = t;
-        int size = sizeof(temps);
-
-        sort(temps, temps + size);
     }
-    for (int i = 0; i < n; i++)
+
+    int size = sizeof(temps) / sizeof(temps[0]);
+    sort(temps, temps + size);
+
+    for (int i = 1; i < n; i++)
     {
+        if (temps[0] > 0)
+        {
+            closestPos = temps[0];
+        }
+        else if (temps[0] < 0)
+        {
+            closestNeg = temps[0];
+        }
+
         if (temps[i] > 0 && temps[i] < temps[i - 1])
         {
             closestPos = temps[i];
@@ -46,11 +57,11 @@ int main()
 
     if (math >= 0)
     {
-        closest = closestPos;
+        closest = closestNeg;
     }
     else if (math < 0)
     {
-        closest = closestNeg;
+        closest = closestPos;
     }
 
     // Write an answer using cout. DON'T FORGET THE "<< endl"
